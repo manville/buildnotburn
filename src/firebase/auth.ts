95c45613e985f5dd29d2ff820917bce6b508a60f
Ryan Manville <manville@gmail.com>
2025-12-06 04:43:28 +0000

    Try fixing this error: `Runtime Error: window is not defined. Error sour

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -4,15 +4,15 @@ import { getAuth, sendSignInLinkToEmail, GoogleAuthProvider, signInWithPopup, Us
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 import { initializeFirebase } from ".";
 
-const actionCodeSettings = {
-  // URL you want to redirect back to. The domain (www.example.com) for this
-  // URL must be in the authorized domains list in the Firebase Console.
-  url: `${window.location.origin}/login`,
-  // This must be true.
-  handleCodeInApp: true,
-};
 
 export const sendSignInLink = async (email: string) => {
+    const actionCodeSettings = {
+        // URL you want to redirect back to. The domain (www.example.com) for this
+        // URL must be in the authorized domains list in the Firebase Console.
+        url: `${window.location.origin}/login`,
+        // This must be true.
+        handleCodeInApp: true,
+    };
     const auth = getAuth();
     await sendSignInLinkToEmail(auth, email, actionCodeSettings);
 };
