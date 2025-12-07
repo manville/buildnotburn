796467b7339898fb8f64f33c5d338512a9f7c23a
Ryan Manville <manville@gmail.com>
2025-12-07 14:46:03 +0000

    404 This page could not be found.

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,39 @@
+
+import type { Metadata } from 'next';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from '@/components/ThemeProvider';
+import Script from 'next/script';
+
+export const metadata: Metadata = {
+  title: 'BuildNotBurn',
+  description: 'The Sustainable System for Long-Term Creators.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Roboto+Mono&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+            {children}
+            <Toaster />
+        </ThemeProvider>
+        <Script async defer src="https://withcabin.com/hello.js" />
+      </body>
+    </html>
+  );
+}
